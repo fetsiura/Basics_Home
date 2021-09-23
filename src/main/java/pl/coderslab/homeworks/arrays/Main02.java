9d85e8c0989e01e01953e46fe4ae93f6e2b499f9
fetsiura <fetsiura@gmail.com>
2021-09-23 21:30:33 +0200

    s2

--- src/main/java/pl/coderslab/homeworks/arrays/Main02.java
+++ src/main/java/pl/coderslab/homeworks/arrays/Main02.java
@@ -1,11 +1,31 @@
 package pl.coderslab.homeworks.arrays;
 
 
+import java.util.Arrays;
+
 public class Main02 {
 
     public static void main(String[] args) {
+        int [] arr = {1,2,3};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(append(arr)));
 
 
     }
+    public static int[] append(int[] arr){
+        int [] ret = new int[arr.length*2];
+        int counter= 0;
+        for (int i=0;i<arr.length;i++){
 
+            ret[counter] = arr[i];
+            counter++;
+            if (counter==arr.length){
+                for (int j = arr.length-1;j>=0;j--){
+                    ret[counter] = arr[j];
+                    counter++;
+                }
+            }
+        }
+        return ret;
+    }
 }
--- src/main/java/pl/coderslab/homeworks/arrays/Main03.java
+++ src/main/java/pl/coderslab/homeworks/arrays/Main03.java
@@ -4,8 +4,23 @@ package pl.coderslab.homeworks.arrays;
 public class Main03 {
 
     public static void main(String[] args) {
+        int [] arr = {1,2,3,4};
+
+//        int check = 1;
+        int check = 5;
+
+        System.out.println(contains(arr,check));
 
 
     }
+    public static boolean contains(int[] arr, int element){
 
+
+        for(int i=0;i<arr.length;i++){
+            if(element==arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
 }
