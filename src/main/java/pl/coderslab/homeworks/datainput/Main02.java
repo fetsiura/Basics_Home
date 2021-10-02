fe51157707a9eaf677ad9c747f101398b8182e1d
fetsiura <fetsiura@gmail.com>
2021-10-02 17:14:23 +0200

    s1

--- src/main/java/pl/coderslab/homeworks/datainput/Main02.java
+++ src/main/java/pl/coderslab/homeworks/datainput/Main02.java
@@ -1,11 +1,35 @@
 package pl.coderslab.homeworks.datainput;
 
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main02 {
 
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(sortedArray()));
+    }
 
+    public static int[] sortedArray(){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("write number");
+        while (!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("Only numbers");
+        }
+
+        int res = scanner.nextInt();
+        int [] arr = new int[res];
+
+        for(int i=0; i<arr.length;i++){
+            arr[i]=random.nextInt(101);
+        }
+        Arrays.sort(arr);
+        return arr;
     }
 
 }
